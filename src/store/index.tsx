03c46f026713645fcc0521dd86f99d43b010e59f
404pandas <mary.panda.jackson@gmail.com>
2025-02-24 17:22:49 -0500

    Redux persisting in local storage

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,14 +2,34 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import searchReducer from "./searchSlice";
 import matchReducer from "./matchSlice";
+import storage from "redux-persist/lib/storage"; // Local storage
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage, // Use local storage
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  search: searchReducer,
+  match: matchReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    search: searchReducer,
-    match: matchReducer,
-  },
+  reducer: persistedReducer, // Use the persisted root reducer
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
