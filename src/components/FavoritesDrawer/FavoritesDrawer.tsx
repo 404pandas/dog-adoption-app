825e3ab3db3e459b00bbad4e70e8fc711b9b500a
404pandas <mary.panda.jackson@gmail.com>
2025-02-24 19:04:29 -0500

    Deploying

--- src/components/FavoritesDrawer/FavoritesDrawer.tsx
+++ src/components/FavoritesDrawer/FavoritesDrawer.tsx
@@ -3,7 +3,7 @@ import "./FavoritesDrawer.css"; // Import the CSS file for the animations
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
 import Match from "../Match/Match";
-import "./drawer.css";
+import "./favoritesDrawer.css";
 
 const FavoritesDrawer = () => {
   const favorites = useSelector((state: RootState) => state.match.favorites);
